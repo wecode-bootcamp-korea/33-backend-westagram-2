0795ee7f99fd0634edc658fa28a7cde2212604cc
박상연 <psy@Sangyeon.local>
2022-05-12 17:06:43 +0900

    Model - user테이블 수정3

--- students/sangyeon/users/models.py
+++ students/sangyeon/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone_number = models.IntegerField(max_length=50)
+class User(models.Model): 
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=50, unique=True)
+    password     = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=50)
+    create_at    = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateField(auto_now=True)
 
-    class Meta:
+    class Meta: 
         db_table = 'users'
 # Create your models here.
