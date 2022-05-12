f807970c1fdc2361038a44e52a0ad24f6220bbf1
박상연 <psy@Sangyeon.local>
2022-05-12 16:39:34 +0900

    Model - user테이블 수정2

--- students/sangyeon/users/models.py
+++ students/sangyeon/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField()
-    email = models.EmailField()
-    password = models.CharField()
-    phone_number = models.IntegerField()
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    phone_number = models.IntegerField(max_length=50)
 
     class Meta:
         db_table = 'users'
