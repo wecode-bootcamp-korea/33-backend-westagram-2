88767fbc1b9b95fa9e86653953398b6283423527
DDinoBox <dinobox.create@gmil.com>
2022-05-13 16:55:17 +0900

    user 테이블 생성_2

--- students/hyein/users/models.py
+++ students/hyein/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+class User(models.Model): 
+    name         = models.CharField(max_length=20)
+    email        = models.EmailField(max_length=200,unique=True)
+    password     = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+    
+    class Meta: 
+        db_table = 'users'
--- students/hyein/users/tests.py
+++ students/hyein/users/tests.py
@@ -1,12 +1,3 @@
-from django.db import models
+from django.test import TestCase
 
-class User(models.Model): 
-    name         = models.CharField(max_length=20)
-    email        = models.EmailField(max_length=200,unique=True)
-    password     = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50)
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
-    
-    class Meta: 
-        db_table = 'users'
+# Create your tests here.
