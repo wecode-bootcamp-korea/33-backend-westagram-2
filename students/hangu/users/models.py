cc08d246c8692a68546b020ae5b8313d2fa5347f
sorkihg <sorkihg@gmail.com>
2022-05-13 15:44:29 +0900

    수정완료2

--- students/hangu/users/models.py
+++ students/hangu/users/models.py
@@ -6,6 +6,8 @@ class User(models.Model):
     user_email   = models.CharField(max_length=60, unique=True)
     password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=13)
+    created_at   = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at   = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         db_table = 'users'
