9088e5f3547fb2ef090c50ed9b46d9e639bbc804
Brian-with <chiyoungv@gmail.com>
2022-05-13 13:07:58 +0900

    (test) charfield unique about upper lower case

--- students/chiyoung/users/models.py
+++ students/chiyoung/users/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.EmailField()
-    password = models.CharField(max_length=45)
-    phone = models.IntegerField()
+class Accounts(models.Model):
+    name         = models.CharField(max_length=45)
+    email        = models.CharField(max_length=45, unique= True) # How to deal with uppercase or lowercase ?
+    password     = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=45)
+    created_at   = models.DateTimeField(auto_now_add=True) # auto_now_add = True
+    updated_at   = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'Users'
+        db_table = 'accounts'
