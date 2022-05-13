d749651a371bce64f2a0acf3db21d8fb9500bcb4
Brian-with <chiyoungv@gmail.com>
2022-05-13 14:03:00 +0900

    [Misiion 3] class SignupView_Version 1

--- students/chiyoung/users/views.py
+++ students/chiyoung/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from .models import Accounts
+
+
+class SignupView(View):
+    
+    def post(self, request):
+        signup_data = json.loads(request.body)
+        account = Accounts.objects.create(
+            name         = signup_data["name"],
+            email        = signup_data["email"],
+            password     = signup_data["password"],
+            phone_number = signup_data["phone_number"],
+            created_at   = signup_data["created_at"],
+            updated_at   = signup_data["updated_at"]
+        )
+        
+        return JsonResponse({"message" : "SUCCESS"}, status = 201)
