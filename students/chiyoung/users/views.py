ef7515ce0163f6d56d6aad9266b962924155e208
Brian-with <chiyoungv@gmail.com>
2022-05-13 15:23:01 +0900

    [Misiion 3] class SignupView_Version 2 : json.decoder.JSONDecodeError 처리완료

--- students/chiyoung/users/views.py
+++ students/chiyoung/users/views.py
@@ -8,20 +8,21 @@ from .models import Accounts
 
 class SignupView(View):
     def post(self, request):
-        signup_data = json.loads(request.body) # dictionary type
         
         try :
-            if Accounts.objects.filter(email=signup_data["email"]).exists():
-                return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
-        
-        except :
+            signup_data = json.loads(request.body) # dictionary type
+        # if Accounts.objects.filter(email=signup_data["email"]).exists():
+        #         return JsonResponse({"message" : "Email address"}, status = 400)
+        # else :
             account = Accounts.objects.create(
-            name         = signup_data["name"],
-            email        = signup_data["email"],
-            password     = signup_data["password"],
-            phone_number = signup_data["phone_number"],
-            created_at   = signup_data["created_at"],
-            updated_at   = signup_data["updated_at"]
-        )
+                name         = signup_data["name"],
+                email        = signup_data["email"],
+                password     = signup_data["password"],
+                phone_number = signup_data["phone_number"],
+                created_at   = signup_data["created_at"],
+                updated_at   = signup_data["updated_at"],
+            )
+        except json.decoder.JSONDecodeError :
+            return JsonResponse({'MESSAGE' : '이건 Json decode error 발생 케이스'}, status = 400)
         
         return JsonResponse({"message" : "SUCCESS"}, status = 201)
