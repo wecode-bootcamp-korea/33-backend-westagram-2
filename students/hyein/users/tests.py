ea8552619d867b894186e71a8e936063da0a0cfe
DDinoBox <dinobox.create@gmil.com>
2022-05-13 16:52:10 +0900

    user 테이블 생성_1

--- students/hyein/users/tests.py
+++ students/hyein/users/tests.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class User(models.Model): 
     name         = models.CharField(max_length=20)
-    email        = models.EmailField(max_length=200)
-    password     = models.CharField(unique=True)
+    email        = models.EmailField(max_length=200,unique=True)
+    password     = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=50)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
