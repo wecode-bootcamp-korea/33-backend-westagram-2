09986d63476ae4f3c06d711d8b076ca0eba096da
Brian-with <chiyoungv@gmail.com>
2022-05-13 15:46:40 +0900

    [Misiion 3] class SignupView_Version 2.1 < History > (version 2.0) json.decoder.JSNDecodeError 처리 완료 (version 2.1) Email Validation 처리 완료

--- students/chiyoung/users/views.py
+++ students/chiyoung/users/views.py
@@ -10,19 +10,20 @@ class SignupView(View):
     def post(self, request):
         
         try :
-            signup_data = json.loads(request.body) # dictionary type
-        # if Accounts.objects.filter(email=signup_data["email"]).exists():
-        #         return JsonResponse({"message" : "Email address"}, status = 400)
-        # else :
-            account = Accounts.objects.create(
-                name         = signup_data["name"],
-                email        = signup_data["email"],
-                password     = signup_data["password"],
-                phone_number = signup_data["phone_number"],
-                created_at   = signup_data["created_at"],
-                updated_at   = signup_data["updated_at"],
-            )
-        except json.decoder.JSONDecodeError :
-            return JsonResponse({'MESSAGE' : '이건 Json decode error 발생 케이스'}, status = 400)
+            signup_data = json.loads(request.body)
         
-        return JsonResponse({"message" : "SUCCESS"}, status = 201)
+            if "@" not in signup_data["email"] or "." not in signup_data["email"] :
+                return JsonResponse({'MESSAGE' : 'INVALID_EMAIL'}, status = 400)
+
+            account = Accounts.objects.create(
+            name         = signup_data["name"],
+            email        = signup_data["email"],
+            password     = signup_data["password"],
+            phone_number = signup_data["phone_number"],
+            created_at   = signup_data["created_at"],
+            updated_at   = signup_data["updated_at"],
+        )
+        except json.decoder.JSONDecodeError :
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+        
+        return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
