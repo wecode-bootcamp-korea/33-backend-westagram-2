bd132debfe4e42c09d1103ef950b3b18c0872e29
sorkihg <sorkihg@gmail.com>
2022-05-14 14:23:51 +0900

    [임한구] Mission 3: 회원가입 기능 구현 : 수정3

--- students/hangu/users/views.py
+++ students/hangu/users/views.py
@@ -15,20 +15,20 @@ class SignupView(View):
             user_email         = user_data["user_email"]
             password           = user_data["password"]
             phone_number       = user_data["phone_number"]
-            regex_email        = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            regex_password     = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
-            regex_phone_number = '^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'
+            REGEX_EMAIL        = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD     = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+            REGEX_PHONE_NUMBER = '^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'
     
             if user_email == "" or password == "":
                 return JsonResponse({"message":"이메일 또는 비밀번호를 입력해주세요"}, status=400)
                 
-            if not re.match(regex_email, user_email):
+            if not re.match(REGEX_EMAIL, user_email):
                 return JsonResponse({"message":"이메일양식이 잘못되었습니다"}, status=400)
 
-            if not re.match(regex_password, password):
+            if not re.match(REGEX_PASSWORD, password):
                 return JsonResponse({"message":"비밀번호는 8자리 이상, 문자, 숫자, 특수문자의 복합이어야합니다. "}, status=400)
             
-            if not re.match(regex_phone_number, phone_number):
+            if not re.match(REGEX_PHONE_NUMBER, phone_number):
                 return JsonResponse({"message":"핸드폰번호가 잘못되었습니다"}, status=400)
 
             email_check = User.objects.filter(user_email = user_email).exists()
@@ -44,6 +44,4 @@ class SignupView(View):
             return  JsonResponse({"message":"이메일이 중복입니다"}, status=400)   
             
         except KeyError:
-            return JsonResponse({"message": 'KeyError'}, status = 400)   
-
-
+            return JsonResponse({"message": 'KeyError'}, status = 400)  
