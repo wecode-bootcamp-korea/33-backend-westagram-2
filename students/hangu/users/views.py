238fb59a11504af84a6acd132c707a7e36c3e023
sorkihg <sorkihg@gmail.com>
2022-05-16 11:31:42 +0900

    수정사항 적용

--- students/hangu/users/views.py
+++ students/hangu/users/views.py
@@ -18,10 +18,7 @@ class SignupView(View):
             REGEX_EMAIL        = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             REGEX_PASSWORD     = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             REGEX_PHONE_NUMBER = '^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$'
-    
-            if user_email == "" or password == "":
-                return JsonResponse({"message":"이메일 또는 비밀번호를 입력해주세요"}, status=400)
-                
+
             if not re.match(REGEX_EMAIL, user_email):
                 return JsonResponse({"message":"이메일양식이 잘못되었습니다"}, status=400)
 
