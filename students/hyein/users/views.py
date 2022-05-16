3633e3cae632768b44134e1eb3ac8ad936ee3f55
DDinoBox <dinobox.create@gmil.com>
2022-05-16 12:57:30 +0900

    회원가입 기능 구현

--- students/hyein/users/views.py
+++ students/hyein/users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# python built-in module
+import json
+import re
 
-# Create your views here.
+# 외부 module
+from django.http import JsonResponse
+from django.views import View
+
+# 내부 module
+from users.models import User
+class SignupView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body) #json -> python dictionary
+            
+            EMAIL_VALIDATION    = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            PASSWORD_VALIDATION = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&^])[A-Za-z\d$@$!%*#?&]{8,}$'
+            
+            email    = data['email']
+            password = data['password']
+            
+            if not re.match(EMAIL_VALIDATION, email): 
+                return JsonResponse({'Message' : 'Invalid Email'}, status = 400)
+            
+            if not re.match(PASSWORD_VALIDATION, password): 
+                return JsonResponse({'Message' : 'Invalid Password'},status = 400)
+
+            if User.objects.filter(email = email).exists(): 
+                return JsonResponse({'Message' : 'Email already in use'}, status = 400)
+    
+            User.objects.create(
+                name         = data['name'],
+                email        = email,
+                password     = password,
+                phone_number = data['phone_number'],
+                )
+            
+            return JsonResponse({'Message': 'CREATED'}, status=201)
+        
+        except: 
+            return JsonResponse({'Message': 'KEY_ERROR'}, status=400)
